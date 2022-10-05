12f8fb72c68976399d7f1d246b33d14e4c56ebfc
pwang17 <patrick.w.phs@gmail.com>
2022-10-05 13:12:15 -0400

    wrote map-attack and TestMapAttack

--- Projects/P3/tests/TestMapAttack.java
+++ Projects/P3/tests/TestMapAttack.java
@@ -3,6 +3,11 @@ import junit.framework.*;
 public class TestMapAttack extends TestCase {
 
   public void testMapAttack() throws FileNotFoundException {
-    return null;
+    NoFrame frame0 = new NoFrame();
+    Location location1 = new Location(6,1);
+    Location location2 = new Location(7,1);  
+    PacMan testPacman = frame0.addPacMan(location1);
+    Ghost testGhost = frame0.addGhost(location2, "ghost", Color.red);
+    assertTrue(frame0.getMap().attack("ghost"));
   }
 }
