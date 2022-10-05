4faf2882ece3cc06538931fb6973c7bfcd314344
Josue Velasquez <josuevel98@gmail.com>
2022-10-05 15:02:23 -0400

    fixed syntax error Mainframe->MainFrame

--- Projects/P3/tests/TestConsume.java
+++ Projects/P3/tests/TestConsume.java
@@ -5,11 +5,8 @@ public class TestConsume extends TestCase {
 
   public void testConsume() throws FileNotFoundException {
       //Creating A Map
-      Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
-      NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
-      //create pacman
+      MainFrame frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
       PacMan pacman = frame.addPacMan(new Location(1, 1)); //Creates PacMan at location x, y
-      frame.initPlayers(); //Creates all of the players
       //Start The Game
       frame.startGame();
       assert(pacman.consume()!=null);
