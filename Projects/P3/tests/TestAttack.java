dcc2c54956f367088d7bbec30dcc18cb52e6b509
shricubed <ishrikar@live.com>
2022-10-18 13:56:49 -0400

    adding noframe

--- Projects/P3/tests/TestAttack.java
+++ Projects/P3/tests/TestAttack.java
@@ -6,7 +6,7 @@ public class TestAttack extends TestCase {
 
   public void testAttack() throws FileNotFoundException {
 
-      MainFrame frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+      NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens Initialized
 
       Ghost ghost = frame.addGhost(new Location(1, 1), "name", Color.red); //Creates a red ghost named "name" at location x,y
       PacMan pacman = frame.addPacMan(new Location(1, 1)); //Creates PacMan at location x, y
