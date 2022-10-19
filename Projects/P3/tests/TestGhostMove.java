e9af61639684fe3b31f40d5d625d0199bbc1a3ca
Josh Wyatt <josh.wyatt197@gmail.com>
2022-10-19 11:14:12 -0400

    pushing ghost move changes test

--- Projects/P3/tests/TestGhostMove.java
+++ Projects/P3/tests/TestGhostMove.java
@@ -6,7 +6,7 @@ public class TestGhostMove extends TestCase {
 
   public void testGhostMove() throws FileNotFoundException {
 
-	  MainFrame frame = new MainFrame();
+	  NoFrame frame = new NoFrame();
 	  Ghost ghost = frame.addGhost(new Location(2, 2), "name", java.awt.Color.red);
 	  assert(ghost.move() == true);
 
